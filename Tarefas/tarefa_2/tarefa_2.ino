92d4ec5a02c236077ebb7b59be73bef85762d5a6
Ronaldojr94 <ronaldojr94r9@gmail.com>
2018-09-30 19:13:14 -0300

    Tarefas 01 e 02

---
+++ Tarefas/tarefa_2/tarefa_2.ino
@@ -0,0 +1,67 @@
+#define LED_PIN 9
+#define BUT_PIN1 7
+#define BUT_PIN2 4
+
+unsigned long TIMESTAMP;
+int ESTADO = 0;
+int DELAY = 1000;
+int buttonLast1 = 1;
+int buttonLast2 = 1;
+
+void setup () {
+Serial.begin(9600);
+pinMode(LED_PIN, OUTPUT); 
+pinMode(BUT_PIN1, INPUT); 
+pinMode(BUT_PIN2, INPUT); 
+}
+void loop () {
+  
+  unsigned long now = millis();
+  if (now >= TIMESTAMP+DELAY) {
+    TIMESTAMP = now;
+    ESTADO = !ESTADO;
+    digitalWrite(LED_PIN, ESTADO);
+  }
+
+  if(debounceButton(BUT_PIN1,buttonLast1) == 0 && buttonLast1 == 1)
+  {
+    
+    DELAY = DELAY + 50;
+    Serial.println(DELAY);
+    buttonLast1 = 0;
+  }
+  else if(debounceButton(BUT_PIN1,buttonLast1) == 1 && buttonLast1 == 0)
+  {
+       buttonLast1 = 1;
+  }
+  if(debounceButton(BUT_PIN2,buttonLast2) == 0 && buttonLast2 == 1)
+  {
+    if((DELAY - 50)>0){
+    DELAY = DELAY - 50;
+    }
+    Serial.println(DELAY);
+    buttonLast2 = 0;
+  }
+  else if(debounceButton(BUT_PIN2,buttonLast2) == 1 && buttonLast2 == 0)
+  {
+       buttonLast2 = 1;
+  }
+
+  
+  if(!digitalRead(BUT_PIN1)&&!digitalRead(BUT_PIN2)&&DELAY<500){
+    digitalWrite(LED_PIN, 0);
+    while(1);
+  }
+}
+
+boolean debounceButton(int buttonPin,boolean buttonLast)
+{
+  boolean stateNow = digitalRead(buttonPin);
+  if(buttonLast!=stateNow)
+  {
+    delay(10);
+    stateNow = digitalRead(buttonPin);
+  }
+  return stateNow;
+  
+}
